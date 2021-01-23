75ffe39bb4805158b23d74544c93621ec1096f32
franky3-git <frankndounga@yahoo.fr>
2021-01-23 10:24:15 +0100

    fetching internally data

--- README.md
+++ README.md
@@ -12,3 +12,4 @@
 				dev: webpack-dev-server
 	5. move index.js to src folder
 	6. change the src script in index.html to ./dist/main.js for build and to main.js for dev
+	7. use the fetch API with the then catch syntax first and with async await after that
--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
 const log = console.log;
 
-log('just to test webpack');
+//fetching data internally
+const url = '../API/api.json';
 
+//then catch syntax
+/*fetch(url)
+.then(response => response.json())
+.then(jsonResponse => {
+	log('this is the data fetched');
+	log(jsonResponse);
+})
+.catch(err => {
+	log('ERROR: ' + err);
+})*/
+
+
+//async await syntax
+async function fetchingData() {
+	const response  = await fetch(url);
+	const jsonResponse = await response.json();
+	log('this is the data fetched');
+	log(jsonResponse);
+}
+
+fetchingData();
 
